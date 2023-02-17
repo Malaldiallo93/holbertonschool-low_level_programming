5f8fc9c0140a27f13bef48e4de56e475cedd2097
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-02-17 06:12:42 -0800

    big size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %1d byte(s)\n",sizeof(char));
-printf("Size of an int: %1d byte(s)\n",sizeof(int));
-printf("Size of a long init: %1d byte(s)\n",sizeof(long));
-printf("Size of a long long init: %1d byte(s)\n",sizeof(long long));
-printf("Size of a float: %1d byte(s)\n",sizeof(float));
+printf("Size of a char: %1d byte(s)\n", sizeof(char));
+printf("Size of an int: %1d byte(s)\n", sizeof(int));
+printf("Size of a long init: %1d byte(s)\n", sizeof(long));
+printf("Size of a long long init: %1d byte(s)\n", sizeof(long long));
+printf("Size of a float: %1d byte(s)\n", sizeof(float));
 return (0);
 }
