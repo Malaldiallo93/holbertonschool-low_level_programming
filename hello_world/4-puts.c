a5834894c0bb6de844aad85510f862af5302a490
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-02-17 03:43:31 -0800

    hey put your head in

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-
-int main() {
-  puts("programming is like a multilingual puzzel");
-  return 0;
-
- }
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("programming is like a multilingual puzzel");
+return (0);
+}
