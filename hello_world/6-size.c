beb16dc0329b1265db85c41b0e35c73271f6b9b9
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-02-17 05:53:16 -0800

    big size

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char:  %1d byte(s)\n", sizeof(char));
+printf("Size of an int:  %1d byte(s)\n", sizeof(int));
+printf("Size of a long init:  %1d byte(s)\n", sizeof(long));
+printf("Size of a long long init:  %1d byte(s)\n", sizeof(long long));
+printf("Size of a float:  %1d byte(s)\n", sizeof(float));
+return (0);
+}
