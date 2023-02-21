1ded92cb76c0a9a28bfb0b469dfeff801b6e841d
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-02-21 06:34:57 -0800

    patience is good

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,14 +11,14 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 0; n >=9; n++)
 	{
 		putchar(n);
-	}
-	if (n != 57)
-	{
-		putchar('.');
-		putchar(' ');
+		if (n != 9)
+		{	
+			putchar('.');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
