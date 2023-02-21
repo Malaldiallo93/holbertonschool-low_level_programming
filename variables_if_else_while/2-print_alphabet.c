d4f55044a765d2ace43bfe6747fd44e7e5b7f37b
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-02-21 03:36:29 -0800

    print your name

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+      
+	int i;
+	        
+	for (i = 0; i < 26; i++)
+        {
+		putchar(alp[i]);
+        }
+	putchar('\n');
+	return (0);
+}
