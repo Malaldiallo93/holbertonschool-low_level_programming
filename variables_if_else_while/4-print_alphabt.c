83b3f5b2e803002526f9e61525b6edea83f9c825
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-02-21 05:24:17 -0800

    all numbers

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 97; i < 123; i++)
+	if (i != 101 && i != 113)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
