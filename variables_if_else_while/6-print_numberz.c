2fdebcd75909be9249d76ce85abc41806f35b6bb
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-02-21 05:10:33 -0800

    get numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:
+ *Numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
