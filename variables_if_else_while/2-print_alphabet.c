41732246da3993f26ba8da6b60e2a0bc14f26ce7
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-02-21 03:42:38 -0800

    print your name

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -9,15 +9,15 @@
  */
 int main(void)
 {
-	
+
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-      
+
 	int i;
-	        
+
 	for (i = 0; i < 26; i++)
-        {
+	{
 		putchar(alp[i]);
-        }
+	}
 	putchar('\n');
 	return (0);
 }
