66a4d14b1a3ab4d398cbd7f7ffd942715f63f807
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-02-22 14:45:28 -0800

    alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 aphabet 10 times
+ *
+ **/
+
+void print_alphabet_x10(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+		char l = 'a';
+
+		while (l <= 'z')
+		{
+			_putchar(l);
+			l++;
+		}
+		d++;
+		_putchar('\n');
+	}
+}
