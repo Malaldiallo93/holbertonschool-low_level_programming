91f03c2822a16402cfa11a5e808c046dadb71f11
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-02-23 09:17:31 -0800

    islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
  * _islower - Shows 1 if lowercase character.
  * Else, shows 0
  *
- * @c: The character in ASCII code 
+ * @c: The character in ASCII code
  *
  * Return: 1 for lowercase. 0 for the rest.
  */
