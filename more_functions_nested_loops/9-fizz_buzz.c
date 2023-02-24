436a60a30fba04467e8683c2538d2d73fe144adf
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-02-24 08:48:34 -0800

    big buzz and fizz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,10 +14,10 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		} else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf(" FizzBuzz");
