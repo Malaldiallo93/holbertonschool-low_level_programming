54972996d77e2bd5b5230bfad61b2935d970455d
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-02-24 06:33:30 -0800

    dont use the _

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers,
+ *
+ * Do not print 2 and 4
+ *
+ * Return: (void)
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			putchar(c);
+	}
+	putchar('\n');
+}
