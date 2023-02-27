49356cb5a77bb2db4eddc8f4967a42913ccd341b
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-02-27 04:18:50 -0800

    my first pointers

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98 - function that takes a pointer to
  * an int as parameter and updates the value it points to to 98
  *
- * @*n: adrress to value to change
+ * @*n: input
  *
  * Return (void)
  */
