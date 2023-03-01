2591bb49e8084b1bc248f34d5a94f012c3e0e7ca
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-03-01 12:41:21 -0800

    print function dest

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,24 +1,23 @@
 #include "main.h"
-
 /**
- * char _strcpy -  function that copies the string pointed to by src
- * @dest: copy to
- * @scr: copy from
- * Return: string
+ * *_strcpy - function that copies the string
+ * pointed to by src, including the terminating
+ * null byte (\0), to the buffer pointed to by dest.
+ *
+ * @dest: The buffer to copy the string to
+ * @src: The string to be copied
+ *
+ * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	int l = 0;
-	int x = 0;
+	int i = 0;
 
-	while (*(src + 1) != '\0')
+	for (; src[i] != '\0'; i++)
 	{
-		l++;
+		dest[i] = src[i];
 	}
-	for (; x < 1; x++)
-	{
-		dest[x] = src[x];
-	}
-	dest[l] = '\0';
+	dest[i] = '\0';
+
 	return (dest);
 }
