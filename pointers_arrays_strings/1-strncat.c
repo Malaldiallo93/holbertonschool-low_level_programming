ee60c5b1b3fc11570a976a595e68e6aceaa63972
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-03-02 05:04:22 -0800

    concatenates

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -8,12 +8,12 @@
  *
  * Return: dest
  */
- char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int len = 0;
 
-	while (dest[len] != '\0' )
+	while (dest[len] != '\0')
 	{
 		len++;
 	}
