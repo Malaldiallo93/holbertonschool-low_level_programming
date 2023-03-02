caa3f12dfc175e2585aae3a093b05ca0d59e3ccb
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-03-02 14:00:03 -0800

    compare string

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,10 +13,8 @@ int _strcmp(char *s1, char *s2)
 
 	for (; s1[i] == s2[i] && s1[i] != '\0'; i++)
 		continue;
-	if (s1[i] < s2[i])
-		return (-1);
-	if (s1[i] > s2[i])
-		return (1);
+	if (s1[i] != s2[i])
+		return (s1[i] - s2[i]);
 	if (s1[i] == s2[i])
 		return (0);
 	return (0);
