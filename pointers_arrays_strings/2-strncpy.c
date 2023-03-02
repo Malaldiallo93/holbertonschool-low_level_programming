4c0ed6373028b1c60f78a9d49d70af6026db77ed
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-03-02 05:58:08 -0800

    copie string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,9 +12,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	for (; i < n; i++)
+	for (; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
+	for (; i < n; i++)
+		dest[i] = '\0';
+	
 	return (dest);
 }
