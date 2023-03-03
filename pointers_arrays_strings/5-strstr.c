adc72d54812b33b0e129d7ba058025e33eabf00d
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-03-03 06:38:34 -0800

    locate substring

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring
+ * @strstr: input
+ * @haystack: a string
+ * @needle: another string
+ * Return: NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+				index++;
+			} while (haystack[index] == needle[index]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
