320488f5c50151a242cc8b4944e8b9189f40c27e
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-03-06 14:01:53 -0800

    function reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: input
+ * Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	} _print_rev_recursion(s + 1);
+	putchar(*s);
+}
