66deb7e6a812ae90c46884fe58fc1d5aa4d49b71
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-03-06 13:42:16 -0800

    my first function recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: input
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
