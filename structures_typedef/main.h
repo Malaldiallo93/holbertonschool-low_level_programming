3549a88c34a03693d1cb11d7cc081fc109772417
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-03-20 06:40:17 -0700

    function typedef

---
+++ structures_typedef/main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
