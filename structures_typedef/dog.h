f929cc96ffc2e928c17b8ed66917fd9225a91dff
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-03-20 06:51:33 -0700

    Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure of  Poppy
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
