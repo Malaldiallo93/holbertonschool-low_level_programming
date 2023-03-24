ac3042dffde36c57903324713f679b450555dc39
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-03-24 11:19:43 -0700

    To be is a to be the value of a variable

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -8,43 +8,44 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list valist;
-	unsigned int i = 0, j, c = 0;
+	int i;
+	int flag;
 	char *str;
-	const char t_arg[] = "cifs";
+	va_list a_list;
 
-	va_start(valist, format);
-	while (format && format[i])
+	va_start(a_list, format);
+	i = 0;
+	while (format != NULL && format[i] != '\0')
 	{
-		j = 0;
-		while (t_arg[j])
-		{
-			if (format[i] == t_arg[j] && c)
-			{	printf(", ");
-				break;
-			} j++;
-		}
 		switch (format[i])
 		{
-		case 'c':
-			printf("%c", va_arg(valist, int)), c = 1;
-			break;
-		case 'i':
-			printf("%d", va_arg(valist, int)), c = 1;
-			break;
-		case 'f':
-			printf("%f", va_arg(valist, double)), c = 1;
-			break;
-		case 's':
-			str = va_arg(valist, char *), c = 1;
-			if (!str)
-			{
-				printf("(nil)");
+			case 'c':
+				printf("%c", va_arg(a_list, int));
+				flag = 0;
 				break;
-			}
-			printf("%s", str);
-			break;
-		} i++;
-		printf("\n"), va_end(valist);
+			case 'i':
+				printf("%d", va_arg(a_list, int));
+				flag = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(a_list, double));
+				flag = 0;
+				break;
+			case 's':
+				str = va_arg(a_list, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				flag = 0;
+				break;
+			default:
+				flag = 1;
+				break;
+		}
+		if (format[i + 1] != '\0' && flag == 0)
+			printf(", ");
+		i++;
 	}
+	printf("\n");
+	va_end(a_list);
 }
