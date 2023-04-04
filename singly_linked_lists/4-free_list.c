704ffc5b65122752583f8c1d070706c9c50afbe7
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-04-04 05:40:16 -0700

    function that frees a list_t list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+	
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+
+}
+
